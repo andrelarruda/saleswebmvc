c5042a0d822b073e83f433093da841f2c06688bf
André Arruda <andre.luizarruda@ufrpe.br>
2020-08-28 18:47:51 -0300

    first commit

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,33 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:MM/dd/yyyy}")]
+        public DateTime Date { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} is required")]
+        [StringLength(maximumLength:60, MinimumLength =3, ErrorMessage ="{0} size should be between {2} and {1}")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [EmailAddress(ErrorMessage = "Type a valid {0}")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name="Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime BirthDate { get; set; }
+        
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(100.0, 50000.00, ErrorMessage = "{0} must be from {1} to {2}")]
+        [Display(Name="Base Salary")]
+        //[DisplayFormat(DataFormatString = "{0:F2}")]
+        [DataType(DataType.Currency)]
+        public double BaseSalary { get; set; }
+        public Department Department  { get; set; }
+        public int DepartmentId { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => (sr.Date >= initial && sr.Date <= final)).Sum(sr => sr.Amount);
+        }
+    }
+}
